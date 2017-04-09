05d315d39687b94141e9cf8d4d552553b1bc7718
gr8seapr0o <gr8seapr0o@users.noreply.github.com>
2017-04-09 22:12:21 +0300

    Add files via upload

---
+++ php_base_devionity_task/22.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SuperUser
+ * Date: 09.04.2017
+ * Time: 19:12
+ */
+define('UKR', 'Ukraine');
+define('IT', 'Italia');
+
+$arr = array(UKR, IT);
+echo '<pre>';
+print_r($arr);
+echo '</pre>';
