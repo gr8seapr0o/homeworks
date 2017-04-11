df1b0f5bd2c5e7ea86f6b1c72152ef24b96c7ec8
gr8seapr0o <mon.to.burn@gmail.com>
2017-04-11 23:23:44 +0300

    Задачи по основам PHP

---
+++ php_basics_tasks/12.php
@@ -0,0 +1,14 @@
+<?php
+
+$day = 8;
+
+switch ($day){
+    case ($day > 1 && $day <= 5 ):
+        echo "Это рабочий день";
+        break;
+    case ($day == 6 || $day == 7):
+        echo "Это выходной день";
+        break;
+    case($day < 1 || $day > 7):
+        echo "Это неизвестный день";
+}
