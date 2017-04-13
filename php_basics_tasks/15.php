3628f72ff8931bfa7f6160a60d889850642030ca
gr8seapr0o <gr8seapr0o@users.noreply.github.com>
2017-04-13 03:48:59 +0300

    simple calc

--- php_basics_tasks/15.php
+++ php_basics_tasks/15.php
@@ -1,1 +1,22 @@
+<?php
+$a = 3;
+$b = 1;
+$operator = "/";
+$res;
+
+if($b == 0 && $operator == "/") {
+	echo "на 0 делить нельзя.";
+	}
+elseif($operator == "*") {
+	echo $res = $a * $b;
+	}
+ elseif($operator == "/") {
+	echo $res = $a / $b;
+	 }
+	 elseif($operator == "-") {
+	echo $res = $a - $b;
+	 }
+	 elseif($operator == "+") {
+	echo $res = $a / $b;
+	 }
 
