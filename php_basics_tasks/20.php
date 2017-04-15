389c2d3490c9866965cf74625fdb728cb28f9e92
gr8seapr0o <mon.to.burn@gmail.com>
2017-04-15 21:37:04 +0300

    задачи 6-12

---
+++ php_basics_tasks/20.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SuperUser
+ * Date: 13.04.2017
+ * Time: 10:49
+ */
---
+++ php_basics_tasks/4.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SuperUser
+ * Date: 11.04.2017
+ * Time: 22:43
+ */
